3248253e1aae58e6ce16103917f27b428e0c2e06
thseo <thseo@spectra.co.kr>
2022-08-03 10:08:59 +0900

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.테르토미노.Solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution s = new Solution();
+        s.test();
+    }
+}
---
+++ src/com/company/테르토미노/Solution.java
@@ -0,0 +1,8 @@
+package com.company.테르토미노;
+
+public class Solution
+{
+    public void test(){
+        System.out.println("HEELO");
+    }
+}
