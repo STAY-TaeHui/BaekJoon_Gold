cf3436d4c2b98808d9a8f1888355f9ac59307101
서태희 <masta95@naver.com>
2022-11-21 16:52:48 +0900

    설탕배달 - DP 진행중

--- src/com/silver/설탕배달_2839/Main.java
+++ src/com/silver/설탕배달_2839/Main.java
@@ -6,14 +6,30 @@ import java.io.InputStreamReader;
 
 //https://www.acmicpc.net/problem/2839
 public class Main {
-    static int dp[];
+    static Integer dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int kg = Integer.parseInt(br.readLine());
+        dp = new Integer[kg];
+        dp[0]=dp[1]=dp[2]=-1;
 
-        if (kg % 5 != 0 || kg % 3 != 0) {
-            System.out.println(-1);
+        System.out.println(recur(kg));
+    }
+    static int recur(int kg){
+        if (dp[kg] == null)
+        {
+            if (kg % 5 == 0)
+            {
+                recur(kg-5);
+            }
+            else if (kg % 3 == 0)
+            {
+                dp[kg]=recur(kg-3);
+            }
+            int min = Math.min(kg % 5, kg % 3);
         }
+        else
+            return dp[kg];
 
 
     }
