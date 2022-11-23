9170a685ecd5245e73fc0b1d097b01dc400b9094
서태희 <masta95@naver.com>
2022-11-23 17:03:11 +0900

    설탕배달 - DP 완료

--- src/com/silver/설탕배달_2839/Main.java
+++ src/com/silver/설탕배달_2839/Main.java
@@ -11,26 +11,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int kg = Integer.parseInt(br.readLine());
         dp = new Integer[kg+1];
-        dp[0]=dp[1]=dp[2]=-1;
 
-        System.out.println(recur(kg));
+        recur(kg);
+        System.out.println(dp[kg]);
     }
     static int recur(int kg)
     {
+        if(kg<0){
+            return -1;
+        }
 
         if (dp[kg] == null)
         {
             if (kg % 5 == 0)
             {
-                dp[kg] = recur(kg - 5);
+                dp[kg] = recur(kg - 5)+1;
+                System.out.println("dp["+kg+"] : " + dp[kg]);
             }
             else if (kg % 3 == 0)
             {
-                dp[kg] = recur(kg - 3);
+                dp[kg] = recur(kg - 3)+1;
+                System.out.println("dp["+kg+"] : " + dp[kg]);
             }
             else
             {
-                dp[kg] = Math.max(recur(kg - 5), recur(kg - 3));
+                int max = Math.max(recur(kg - 5), recur(kg - 3));
+                dp[kg] = max<0 ? -1 : max+1;
+                System.out.println("dp["+kg+"] : " + dp[kg]);
+
             }
         }
         return dp[kg];
